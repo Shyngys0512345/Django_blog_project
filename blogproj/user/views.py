88a71d5faef20d95e14962f0bf1aac9ed295ccbe
Shyngys <sh_yeszhan@kbtu.kz>
2024-10-13 21:22:08 +0500

    commit

---
+++ blogproj/user/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm   
+from django.contrib import messages
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
+from django.contrib.auth.decorators import login_required 
+
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
+from .models import Profile
+from .forms import UserUpdateForm, ProfileUpdateForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Successfully Created for {username} Login In Now!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
+   
+
+
+def profile_update(request):
+    # Ensure the profile exists or create one
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = Profile.objects.create(user=request.user)
+
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your Profile was Updated!!!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'user/profile_update.html', context)
+    
